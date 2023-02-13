36427afa73ef7333d1b95a41801681c77d9eac59
Jashanpreet Singh <102400435+jashan75279@users.noreply.github.com>
2023-02-13 21:13:10 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>JashanYaar.</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/jashan75279" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
